d19627a616ab75c6aad3940aafc647e560f110f7
Gülseli Ocakcı <154464410+gulseliocakci@users.noreply.github.com>
2024-12-20 22:52:04 +0300

    Summary

---
+++ src/main/java/org/example/Interface.java
@@ -0,0 +1,12 @@
+package org.example;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Interface {
+    JFrame frame = new JFrame("Online Cargo Tracking System");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setExtendedState(JFrame.MAXIMIZED_BOTH);  // Tam ekran yapmak için
+    frame.setVisible(true);
+}
